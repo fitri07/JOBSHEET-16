41838086e8a8a31d1260a7139e97c8b580c2d2d0
luluk05 <luluk4696@gmail.com>
2022-11-15 08:58:38 +0700

    first commit

---
+++ app/src/main/java/com/example/rahmajob16/ActivityDialog.kt
@@ -0,0 +1,11 @@
+package com.example.rahmajob16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog)
+    }
+}
